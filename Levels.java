c0f5c19370d7d8acc307b1af631066e30d7d3e31
baugh70 <zakkyzakkba@gmail.com>
2013-11-18 10:56:08 -0500

    Created loadConfiguration()

--- Levels.java
+++ Levels.java
@@ -1,2 +1,19 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class Level{
+   public void onEnable(){
+     
+   }
+   
+   public void onDisable(){
+     
+   }
+   
+   public void loadConfiguration(){
+     getConfig().options().header("Levels Plugin!");
+     getConfig().addDefault("Food_Levels", true);
+     
+   }
+  
+  
 }
