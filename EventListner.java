e0f1eae2d079d12f3cb4ef24b63224951c5fa429
baugh70 <zakkyzakkba@gmail.com>
2013-11-20 10:54:03 -0500

    Update EventListner.java

--- EventListner.java
+++ EventListner.java
@@ -1,3 +1,15 @@
 public class EventListner{
+  
+  public void onEntityDamageByEntity((Entity damager, Entity damagee, EntityDamageEvent.DamageCause cause, double damage){
+    if(attackLevels == true && damager.hasPermission("levels.attacklevels")){ // If enabled in configuration
+      if(damager instanceof Player){ // check to see if the damager
+        int level = damager.getLevel();
+        double levelDamage = level * attackIncrease;
+        double newDamage = damage + levelDamage;
+        
+        damgee.setDamage(newDamage);
+      }
+    }
+  }
 
 }
